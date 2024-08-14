d10f972aabe5c08fba51e74bfce0807644489569
Aimable Ruhumuliza <phyntom@gmail.com>
2024-08-14 15:57:16 -0400

    handling async merge and print

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,45 @@
-"use strict";
+'use strict';
+
+const MinHeap = require('./min-heap');
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+   return new Promise(async (resolve, reject) => {
+      await printMergeLogsAsync(logSources, printer)
+         .then(() => {
+            console.log('Async sort complete.');
+            resolve();
+         })
+         .catch(reject);
+   });
 };
+// method to print the logs using async methods
+async function printMergeLogsAsync(logSources, printer) {
+   // start by creating min heap
+   const heap = new MinHeap();
+   try {
+      await Promise.all(
+         await logSources.map(async (source, idx) => {
+            const record = await source.popAsync();
+            if (record) {
+               heap.insert({ sourceIndex: idx, record });
+            }
+         })
+      );
+      while (heap.size() > 0) {
+         const { sourceIndex, record } = heap.extractMin();
+         printer.print(record);
+         // Get the next entry from the same source
+         const nextRecord = await logSources[sourceIndex].popAsync();
+         if (nextRecord) {
+            heap.insert({ sourceIndex, record: nextRecord });
+         } else {
+            printer.done();
+            break;
+         }
+      }
+   } catch (err) {
+      console.error(err);
+   }
+}
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,98 +1,38 @@
 'use strict';
 const fs = require('fs');
+const MinHeap = require('./min-heap');
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
    if (!logSources.length || logSources.length > 0) {
-      mergeLogsSynchronous(logSources, printer);
+      printLogsSynchronous(logSources, printer);
       printer.done();
    }
    return console.log('Sync sort complete.');
 };
 
-function mergeLogsSynchronous(logSources, printer) {
+function printLogsSynchronous(logSources, printer) {
    const heap = new MinHeap();
    // Initialize the heap with the first entry from each source
    for (let i = 0; i < logSources.length; i++) {
       // Since the source record are poped chronologically, we have guarantee that
       // records will come already sorted from the source
-      const entry = logSources[i].pop();
-      if (entry) {
-         heap.insert({ sourceIndex: i, entry }); // each source is marked with index
+      const record = logSources[i].pop();
+      if (record) {
+         heap.insert({ sourceIndex: i, record }); // each record is marked with its source id
       }
    }
 
    // Process entries
    while (heap.size() > 0) {
-      const { sourceIndex, entry } = heap.extractMin();
-      printer.print(entry);
-      // Get the next entry from the same source
-      const nextEntry = logSources[sourceIndex].pop();
-      if (nextEntry) {
-         heap.insert({ sourceIndex, entry: nextEntry });
+      // extract the minimum and its source index to be later replaced
+      const { sourceIndex, record } = heap.extractMin();
+      printer.print(record);
+      // get the follwing entry from the same source which the minimum was take from
+      const nextRecord = logSources[sourceIndex].pop();
+      if (nextRecord) {
+         heap.insert({ sourceIndex, record: nextRecord });
       }
-   }
-}
-// Implementation of Min-Heap with percolateUp [ known as bubbleUp ] / percolateDown [ known as bubbleDown ]
-class MinHeap {
-   constructor() {
-      this.heap = [];
-   }
-
-   insert(item) {
-      this.heap.push(item);
-      this.percolateUp(this.heap.length - 1);
-   }
-
-   extractMin() {
-      if (this.heap.length === 0) return null;
-      if (this.heap.length === 1) return this.heap.pop();
-
-      const min = this.heap[0];
-      this.heap[0] = this.heap.pop();
-      this.percolateDown(0);
-      return min;
-   }
-
-   percolateUp(index) {
-      while (index > 0) {
-         const parentIndex = Math.floor((index - 1) / 2);
-         if (this.heap[parentIndex].entry.date <= this.heap[index].entry.date) break;
-         //  swap the value of the heap
-         const tmp = this.heap[index];
-         this.heap[index] = this.heap[parentIndex];
-         index = parentIndex;
-      }
-   }
-
-   percolateDown(index) {
-      while (true) {
-         let smallest = index;
-         const leftChild = 2 * index + 1;
-         const rightChild = 2 * index + 2;
-
-         if (
-            leftChild < this.heap.length &&
-            this.heap[leftChild].entry.date < this.heap[smallest].entry.date
-         ) {
-            smallest = leftChild;
-         }
-         if (
-            rightChild < this.heap.length &&
-            this.heap[rightChild].entry.date < this.heap[smallest].entry.date
-         ) {
-            smallest = rightChild;
-         }
-
-         if (smallest === index) break;
-
-         [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
-         index = smallest;
-      }
-   }
-
-   size() {
-      return this.heap.length;
    }
 }
